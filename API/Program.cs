151eac41e7a48a1d5975b8e2ff0a32a1fa4d0315
Se7s69 <mohamed2001hussein@gmail.com>
2024-09-12 14:12:02 +0200

    Initial 2 Commit

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//Hello this is the main program
 namespace API
 {
     public class Program
