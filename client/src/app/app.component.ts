1f2b108f9a1b8caf1701d65fbe16be8218275cc7
Se7s69 <mohamed2001hussein@gmail.com>
2024-09-21 15:47:45 +0200

    designing of the ui, adding sorting filtering searching pagination and etc

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/products';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/products';
+import { IPagination } from './shared/models/pagination';
+
 
 @Component({
   selector: 'app-root',
@@ -11,17 +12,8 @@ import { IPagination } from './models/pagination';
 export class AppComponent implements OnInit {
 
   title = 'MyApp';
-  products: IProduct[];
-
-  constructor(private http: HttpClient) {}
-
+  constructor() {}
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50')
-    .subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, 
-    error => {
-      console.log(error)
-    })
+
   }
 }
